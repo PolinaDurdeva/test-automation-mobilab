a4af13602e8f2fc5cfc366940e9a5a78c0065989
polinadiurdeva <2696668pol>
2021-01-03 09:39:04 +0100

    added Endpoint class

---
+++ api-test/src/main/java/de/mobilab/api/services/Endpoint.java
@@ -0,0 +1,12 @@
+package de.mobilab.api.services;
+
+public class Endpoint {
+
+    public final static String ACCOUNTS = "/accounts";
+    public final static String ACCOUNT = "/accounts/{id}";
+    public final static String TRANSACTIONS = "/transactions";
+
+    private Endpoint() {
+    }
+
+}

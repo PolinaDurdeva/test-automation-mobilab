d44d771e576c9f27bdc75f0b680e3e91f4683030
polinadiurdeva <2696668pol>
2021-01-06 18:53:28 +0100

    created test class: AccountListTest

---
+++ ui-test/src/test/java/de/mobilab/ui/tests/AccountListTest.java
@@ -0,0 +1,39 @@
+package de.mobilab.ui.tests;
+
+import com.codeborne.selenide.CollectionCondition;
+import de.mobilab.api.currency.Currency;
+import de.mobilab.api.dataprovider.AccountDataProvider;
+import de.mobilab.api.dto.Account;
+import de.mobilab.ui.pages.MainPage;
+import io.qameta.allure.Description;
+import java.math.BigDecimal;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AccountListTest extends BaseTest {
+
+    private final AccountDataProvider accountDataProvider = new AccountDataProvider();
+    private Account account;
+
+    @BeforeClass
+    public void createAccount() {
+        account = accountDataProvider.createAccount("A", BigDecimal.valueOf(1000.1), Currency.EURO);
+    }
+
+    @Test
+    @Description("New accounts appears in the account list with correct data")
+    public void createdAccountDisplayedInTable() {
+        MainPage.open()
+                .goToAccountList()
+                .findAccount(account.getId())
+                .orElseThrow(() -> new AssertionError("Account #id" + account.getId() + "not found"))
+                .shouldHave(CollectionCondition.texts(
+                account.getId(),
+                account.getOwner(),
+                account.getBalance().toString(),
+                account.getCurrency().name(),
+                ""
+        ));
+    }
+
+}

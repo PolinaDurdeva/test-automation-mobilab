c7e64f8512b4fde6d0862d22301083426761054f
polinadiurdeva <2696668pol>
2021-01-06 18:38:56 +0100

    added requests to AccountService

--- api-test/src/main/java/de/mobilab/api/services/AccountService.java
+++ api-test/src/main/java/de/mobilab/api/services/AccountService.java
@@ -19,4 +19,20 @@ public class AccountService extends ApiService {
                 .post(Endpoint.ACCOUNTS));
     }
 
+    @Step("Get an account with id: {id}")
+    public AssertableResponse getAccount(String id) {
+        return new AssertableResponse(RestAssured.given()
+                .spec(specification)
+                .get(Endpoint.ACCOUNT, id));
+    }
+
+    @Step("Get account list with params: page={page}, size={size}")
+    public AssertableResponse getAccounts(int page, int size) {
+        return new AssertableResponse(RestAssured.given()
+                .spec(specification)
+                .queryParam("page", page)
+                .queryParam("size", size)
+                .get(Endpoint.ACCOUNTS));
+    }
+
 }
--- api-test/src/main/java/de/mobilab/api/services/ApiService.java
+++ api-test/src/main/java/de/mobilab/api/services/ApiService.java
@@ -1,5 +1,6 @@
 package de.mobilab.api.services;
 
+import de.mobilab.api.services.specifications.BaseSpecification;
 import io.restassured.specification.RequestSpecification;
 
 public abstract class ApiService {

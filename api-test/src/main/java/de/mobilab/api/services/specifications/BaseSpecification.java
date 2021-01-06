f55d0e1080a53fbba9ab9b03167f54affb451e56
polinadiurdeva <2696668pol>
2021-01-06 18:39:31 +0100

    contentType for BaseSpecification

--- api-test/src/main/java/de/mobilab/api/services/specifications/BaseSpecification.java
+++ api-test/src/main/java/de/mobilab/api/services/specifications/BaseSpecification.java
@@ -1,8 +1,9 @@
-package de.mobilab.api.services;
+package de.mobilab.api.services.specifications;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,6 +21,7 @@ public class BaseSpecification implements Specification {
         RequestSpecification spec = new RequestSpecBuilder()
                 .log(LogDetail.URI)
                 .addFilter(new AllureRestAssured())
+                .setContentType(ContentType.JSON)
                 .build();
         LOGGER.info("Request specification is ready");
         return spec;
